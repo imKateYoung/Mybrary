ae78c3f9ead6b5d04c7486123e901c259a6895ef
imkateyoung <imkateyoungdev@gmail.com>
2021-09-15 22:25:51 -0400

    finish project

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,14 +1,30 @@
+const rootStyle = window.getComputedStyle(document.documentElement)
 
-FilePond.registerPlugin(
-    FilePondPluginImagePreview,
-    FilePondPluginImageResize,
-    FilePondPluginFileEncode,
-)
+if (rootStyle.getPropertyValue('--book-cover-width-large') != null && rootStyle.getPropertyValue('--book-cover-width-large') != '') {
+    ready()
+}
+else{
+    document.getElementById('main-css').addEventListener('load',ready)
+}
 
-FilePond.setOptions({
-    stylePanelAspectRatio: 150 / 100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
-})
+function ready(){
 
-FilePond.parse(document.body);
+    const coverWidth = parseFloat(rootStyle.getPropertyValue('--book-cover-width-large'))
+    const coverAspectRatio = parseFloat(rootStyle.getPropertyValue('--book-cover-aspect-ratio'))
+    const coverHeight = coverWidth / coverAspectRatio
+
+    FilePond.registerPlugin(
+        FilePondPluginImagePreview,
+        FilePondPluginImageResize,
+        FilePondPluginFileEncode,
+    )
+
+    FilePond.setOptions({
+        stylePanelAspectRatio:  1 /coverAspectRatio,
+        imageResizeTargetWidth: coverWidth,
+        imageResizeTargetHeight: coverHeight
+    })
+
+    FilePond.parse(document.body);
+}
+
